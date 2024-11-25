6221558e1576ce114df27209ec969e4863aaad66
Iman Maulana <maulanaiman902@gmail.com>
2024-11-25 16:25:07 +0700

    feat: add TooltipProver

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 interface ProvidersProps
   extends React.ComponentProps<typeof NextThemesProvider> {
@@ -16,10 +17,12 @@ export default function Providers({ children, ...props }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <NextThemesProvider {...props}>
-        {process.env.NODE_ENV === 'development' && (
-          <ReactQueryDevtools initialIsOpen={false} />
-        )}
-        {children}
+        <TooltipProvider>
+          {process.env.NODE_ENV === 'development' && (
+            <ReactQueryDevtools initialIsOpen={false} />
+          )}
+          {children}
+        </TooltipProvider>
       </NextThemesProvider>
     </QueryClientProvider>
   );
